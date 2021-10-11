06ee8aa4739a5d59b76c8943186174b0b732fc8f
Luis Mayta <luis@hadenlabs.com>
2021-10-11 17:13:40 -0500

    chore: implement module aws key (#10)

--- test/instance-basic-with-docker/main.tf
+++ test/instance-basic-with-docker/main.tf
@@ -1,6 +1,6 @@
 module "tags" {
   source      = "hadenlabs/tags/null"
-  version     = "0.1.1"
+  version     = ">=0.1"
   namespace   = var.namespace
   environment = var.environment
   stage       = var.stage
@@ -8,12 +8,28 @@ module "tags" {
   tags        = var.tags
 }
 
+module "key" {
+  source     = "hadenlabs/key-pair/aws"
+  version    = ">=0.1"
+  name       = module.tags.name
+  tags       = module.tags.tags
+  public_key = var.public_key
+  depends_on = [
+    module.tags,
+  ]
+}
+
+
 module "main" {
   source         = "../.."
   name           = module.tags.name
   ami            = data.aws_ami.amazon_linux.id
   tags           = module.tags.tags
-  enabled_docker = var.enabled_docker
-  public_key     = var.public_key
   private_key    = var.private_key
+  enabled_docker = var.enabled_docker
+  aws_key        = module.key.name
+  depends_on = [
+    module.tags,
+    module.key,
+  ]
 }
